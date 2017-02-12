d01b1886991419e113fda6e46b99a012141537c9
kevinrodrigues <Kevin@kevins-MBP.home>
2017-02-12 19:42:15 +0000

    Added vuejs to git.

---
+++ app.js
@@ -0,0 +1,39 @@
+new Vue({
+	el: '#events',
+	data: {
+		event: {
+			name: '',
+			description: '',
+			date: ''
+		},
+		events: []
+	},
+
+	ready: function() {
+		this.getEvents();
+	},
+
+	methods: {
+		getEvents: function(eventType) {
+			eventType = [
+				{
+					id: 1,
+					name: 'Event',
+					description: 'Event description',
+					date: '2017-09-11'
+				}
+			];
+
+			this.$set('events', events);
+		},
+
+		addEvent: function() {
+			// console.log(this.event.description);
+			if (this.event.name) {
+				this.events.push(this.event);
+				this.event = { name: '', description: '', date: '' };
+			}
+
+		}
+	}
+});
